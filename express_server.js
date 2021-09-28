51aa23c55c07017c5b27bc6eb28276fa6842dbfc
Madeleine <m.monica6889@gmail.com>
2021-09-28 09:55:59 -0400

    urls_new

--- express_server.js
+++ express_server.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -11,11 +18,18 @@ app.get("/urls", (req, res) => {
     const templateVars = { urls :urlDatabase };
     res.render("urls_index", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
 app.get("/urls/:shortURL", (req, res) => {
     const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
     res.render("urls_show", templateVars);
 });
-
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -32,4 +46,7 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+
 
