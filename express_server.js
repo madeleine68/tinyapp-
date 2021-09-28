97eb58d37bf587f3a4de98149dfd37853b12f281
Madeleine <m.monica6889@gmail.com>
2021-09-27 22:21:56 -0400

    add header

--- express_server.js
+++ express_server.js
@@ -7,6 +7,14 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.get("/urls", (req, res) => {
+    const templateVars = { urls :urlDatabase };
+    res.render("urls_index", templateVars);
+});
+app.get("/urls/:shortURL", (req, res) => {
+    const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+    res.render("urls_show", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
